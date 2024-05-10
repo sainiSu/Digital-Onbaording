d2a50d86827a9151a38b9166ee293777dce45fb1
student-selflearenr <gunisu3@gmail.com>
2024-05-10 16:30:53 +0200

    Ejercicio-4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,36 @@
-export default function Card({ data , handleStep}) {
+export default function Card({ data, handleStep, handlePrevStep }) {
 
-    function handleClick(){
-        handleStep()
-    }
-
-
-  return (
-    <div className="card">
-       
-       <div className="card-header">
-        <img src={data.image} alt="" className="card-image" />
-
-       </div>
-
-       <div className="card-body">
-
-        <h1 className="card-title">{data.title}</h1>
-        <p>{data.description}</p>
-
-       </div>
-
-      <button className="card-btn" onClick={ handleClick}>&#11166;</button>
-
-    </div>
-  )
-}
+    return (
+      <div className="card">
+  
+        <div className="card-header">
+  
+          <img src={data.image} alt="" className="card-image" />
+  
+        </div>
+  
+        <div className="card-body">
+  
+          <h1 className="card-title">{data.title}</h1>
+          <p>{data.description}</p>
+  
+        </div>
+  
+        <div className="buttons">
+          
+          <div className="indicator">
+  
+            <ul>
+              <li>&#11044;</li>
+              <li>&#11044;</li>
+              <li>&#11044;</li>
+            </ul>
+          </div>
+        <div>
+          <button className="card-btn2" onClick={handlePrevStep}>&#11164;</button>
+          <button className="card-btn" onClick={handleStep}>&#11166;</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
